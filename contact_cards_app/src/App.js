4f603242363dbd19611c18f26f466a85a972089e
Tatjana Svjatova <tsv.mailbox@gmail.com>
2020-06-02 16:23:47 +0300

    hook useEffect

--- contact_cards_app/src/App.js
+++ contact_cards_app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.css";
 
 // const App = () => {
@@ -17,17 +17,52 @@ import "./styles.css";
 // };
 
 const App = () => {
+  // const contacts = [
+  //   { name: "Jenny Han", email: "jenny.han@notreal.com", age: 25 },
+  //   { name: "Jason Long", email: "jason.long@notreal.com", age: 45 },
+  //   { name: "Peter Pan", email: "peter.pan@neverland.com", age: 100 },
+  // ];
+  const [contacts, setContacts] = useState([]);
+  {
+    /*useEffect hook runs on every re-render. However, we can 
+configure it to only run under certain condition, e.g. when a 
+component mounts, or if a variable changes.
+If we want to specify “only run once” we pass in an empty 
+array as a second argument.
+  */
+  }
+  useEffect(() => {
+    fetch("https://randomuser.me/api/?results=3")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setContacts(data.results);
+      });
+  }, []);
   return (
     <>
+      {/*we map over the array. For each object in the array, 
+    we want to create a new ContactCard component.*/}
+      {contacts.map((contact) => (
+        <ContactCard
+          // avatar="https://via.placeholder.com/150"
+          avatar={contact.picture.large}
+          name={`${contact.name.first} ${contact.name.last} `}
+          // name={contact.name}
+          email={contact.email}
+          age={contact.dob.age}
+          // age={contact.age}
+        />
+      ))}
       {/* <ContactCard /> */}
-      <ContactCard avatar="https://via.placeholder.com/150" name="Jenny Han" email="jenny.han@notreal.com" age={25} />
+      {/* <ContactCard avatar="https://via.placeholder.com/150" name="Jenny Han" email="jenny.han@notreal.com" age={25} />
       <ContactCard avatar="https://via.placeholder.com/150" name="Jason Long" email="jason.long@notreal.com" age={45} />
       <ContactCard
         avatar="https://via.placeholder.com/150"
-        name="Peter Pan"
+        name="Peter Pan";
         email="peter.pan@neverland.com"
         age={100}
-      />
+      /> */}
     </>
   );
 };
